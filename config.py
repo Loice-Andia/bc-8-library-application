dfcd15f3222eaffd65863b023137f46ad1633faf
Loice-Andia <loiceandia@gmail.com>
2016-06-24 00:52:39 +0300

    Config to make database url uniform

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-os.environ['DATABASE_URL'] = "postgresql://lolo:lolo@localhost:5432/library_application"
+
 
 
 class Config:
@@ -8,6 +8,8 @@ class Config:
     that is common in all environments
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'loloaloki'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     @staticmethod
     def init_app(app):
@@ -20,18 +22,15 @@ class DevelopementConfig(Config):
     '''
     DEBUG = True
     DEBUG_TB_INTERCEPT_REDIRECTS = False
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    
+    
 
 
 class ProductionConfig(Config):
     '''This class cofigures the production
     environment properties
     '''
-    PORT = int(os.environ.get("PORT", 5000))
-    HOST = '0.0.0.0'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    
     DEBUG = False
 
 
